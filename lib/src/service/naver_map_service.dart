f1a5eb82274633d37a937f18e2022fdad3b3a454
itaebro <plus100kt@gmail.com>
2022-09-30 17:00:24 +0900

    feat: 네비게이션 서비스 추가

--- lib/src/service/naver_map_service.dart
+++ lib/src/service/naver_map_service.dart
@@ -3,9 +3,10 @@ import 'package:ridingpartner_flutter/src/utils/user_location.dart';
 import 'package:http/http.dart' as http;
 import 'dart:developer' as developer;
 import '../models/place.dart';
+import '../models/route.dart';
 
 class NaverMapService {
-  final String naverMapUrl = "map.naver.com";
+  final String _naverMapUrl = "map.naver.com";
 
   Future<List<Place>?> getPlaces(String title) async {
     try {
@@ -17,7 +18,7 @@ class NaverMapService {
       };
 
       final requestUrl =
-          Uri.https(naverMapUrl, '/v5/api/instantSearch', queryParams);
+          Uri.https(_naverMapUrl, '/v5/api/instantSearch', queryParams);
       var response = await http.get(requestUrl);
       developer.log(requestUrl.toString());
 
@@ -37,6 +38,47 @@ class NaverMapService {
         return place;
       } else {
         return place;
+      }
+    } catch (e) {
+      developer.log(e.toString());
+      throw Exception(e.toString());
+    }
+  }
+
+  Future<List<Guide>?> getRoute(
+      Place start, Place destination, List<Place>? waypoints) async {
+    try {
+      var guides = [Guide()];
+
+      String placeToParam(Place place) =>
+          '${place.longitude},placeid=${place.latitude},name=${place.id}';
+
+      final Map<String, String> queryParams = {
+        'start': placeToParam(start),
+        'destination': placeToParam(destination),
+      };
+
+      if (waypoints!.isNotEmpty) {
+        queryParams['waypoints'] = waypoints!.map(placeToParam).join('|');
+      }
+
+      final requestUrl =
+          Uri.https(_naverMapUrl, 'v5/api/dir/findbicycle', queryParams);
+      var response = await http.get(requestUrl);
+      developer.log(requestUrl.toString());
+
+      if (response.statusCode == 200) {
+        var jsonResponse = jsonDecode(response.body);
+        var routeData = NaverRouteData.fromJson(jsonResponse);
+        guides = routeData.routes!
+            .expand<Legs>((rou) => rou.legs!)
+            .expand((leg) => leg.steps!)
+            .map((step) => step.guide!)
+            .toList();
+
+        return guides;
+      } else {
+        return guides;
       }
     } catch (e) {
       developer.log(e.toString());
