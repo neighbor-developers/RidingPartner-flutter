a41914b1666e1f0abe2565bdd21b7137b72f5c64
bjk1649 <qhwns1649@naver.com>
2022-10-04 16:19:28 +0900

    fix: setUserIsNull 관련 주석처리, 실행가능한 상태

--- lib/src/provider/auth_provider.dart
+++ lib/src/provider/auth_provider.dart
@@ -17,23 +17,23 @@ class AuthProvider with ChangeNotifier {
 
   void _setUser(User? user) {
     _user = user;
-    _setUserIsNull();
+    // _setUserIsNull();
     notifyListeners();
   }
 
   void prepareUser() {
     _user = fAuth.currentUser;
-    _setUserIsNull();
+    // _setUserIsNull();
   }
 
-  void _setUserIsNull() {
-    if (_user == null) {
-      _userIsNull = false;
-    } else {
-      _userIsNull = true;
-    }
-    notifyListeners();
-  }
+  // void _setUserIsNull() {
+  //   if (_user == null) {
+  //     _userIsNull = false;
+  //   } else {
+  //     _userIsNull = true;
+  //   }
+  //   notifyListeners();
+  // }
 
   signInWithKakao() async {
     User? user = await socialLogin.signInWithKakao();
