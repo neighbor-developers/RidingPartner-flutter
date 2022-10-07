ab9e68ca978c38048882eb55343c904f0a752624
bjk1649 <qhwns1649@naver.com>
2022-10-07 18:13:27 +0900

    test: 검색결과 클릭시 카메라 이동기능 테스트

--- lib/src/pages/map_page.dart
+++ lib/src/pages/map_page.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
+import 'package:ridingpartner_flutter/src/models/place.dart';
+import 'package:ridingpartner_flutter/src/utils/user_location.dart';
 import 'dart:developer' as developer;
 import '../provider/map_search_provider.dart';
 
@@ -14,32 +16,30 @@ class MapSample extends StatefulWidget {
 
 class MapSampleState extends State<MapSample> {
   final Completer<GoogleMapController> _controller = Completer();
-  var _myController;
-  static final CameraPosition _initLocation = CameraPosition(
+  var _initLocation = const CameraPosition(
     target: LatLng(37.5665, 126.9780),
     zoom: 14.4746,
   );
-
-  // static final CameraPosition _kLake =  CameraPosition(
-  //     bearing: 192.8334901395799,
-  //     target: LatLng(37.43296265331129, -122.08832357078792),
-  //     tilt: 59.440717697143555,
-  //     zoom: 19.151926040649414);
+  List<Marker> _markers = [];
+  @override
+  void initState() {
+    super.initState();
+    _initLoaction();
+  }
 
   @override
   Widget build(BuildContext context) {
     final mapSearchProvider = Provider.of<MapSearchProvider>(context);
-    bool _visibility = false;
 
     return Scaffold(
       body: Stack(
         children: <Widget>[
           GoogleMap(
             mapType: MapType.normal,
+            markers: Set.from(_markers),
             initialCameraPosition: _initLocation,
             onMapCreated: (GoogleMapController controller) {
               _controller.complete(controller);
-              _myController = controller;
             },
             myLocationButtonEnabled: true,
             myLocationEnabled: true,
@@ -71,14 +71,6 @@ class MapSampleState extends State<MapSample> {
                       heroTag: 'searchBtn',
                       onPressed: () async {
                         await mapSearchProvider.setSearchResult("시청");
-                        final GoogleMapController controller =
-                            await _controller.future;
-                        controller.animateCamera(CameraUpdate.newCameraPosition(
-                          const CameraPosition(
-                            target: LatLng(37.4, 126.7),
-                            zoom: 14.4746,
-                          ),
-                        ));
                       },
                       label: const Text('검색'),
                       icon: const Icon(Icons.search),
@@ -114,9 +106,22 @@ class MapSampleState extends State<MapSample> {
                       return ListTile(
                           title: Text(
                               mapSearchProvider.searchResult[index].title!),
-                          onTap: () {
-                            developer.log('tap$index');
-                            // code for set destination
+                          onTap: () async {
+                            final GoogleMapController controller =
+                                await _controller.future;
+                            controller
+                                .animateCamera(CameraUpdate.newCameraPosition(
+                              CameraPosition(
+                                target: LatLng(
+                                    double.parse(mapSearchProvider
+                                        .searchResult[index].longitude!),
+                                    double.parse(mapSearchProvider
+                                        .searchResult[index].latitude!)),
+                                zoom: 20,
+                              ),
+                            ));
+                            _updatePosition(
+                                mapSearchProvider.searchResult[index]);
                           });
                     },
                   ),
@@ -129,7 +134,37 @@ class MapSampleState extends State<MapSample> {
     );
   }
 
+  void _updatePosition(Place position) {
+    if (_markers[0] != null) {
+      _markers.removeAt(0);
+    }
+    _markers.add(
+      Marker(
+        markerId: MarkerId('1'),
+        position: LatLng(double.parse(position.latitude!),
+            double.parse(position.longitude!)),
+        draggable: true,
+      ),
+    );
+    setState(() {});
+  }
+
   Future<void> _goBackToMain() async {
     Navigator.pop(context);
+  }
+
+  void _initLoaction() async {
+    final myLocation = MyLocation();
+    await myLocation.getMyCurrentLocation();
+    _initLocation = CameraPosition(
+      target: LatLng(myLocation.latitude!, myLocation.longitude!),
+      zoom: 14.4746,
+    );
+    _markers.add(Marker(
+        markerId: const MarkerId("1"),
+        draggable: true,
+        onTap: () => print("Marker!"),
+        position: LatLng(myLocation.latitude!, myLocation.longitude!)));
+    developer.log("init Location called");
   }
 }
--- lib/src/provider/map_search_provider.dart
+++ lib/src/provider/map_search_provider.dart
@@ -6,10 +6,13 @@ import 'dart:developer' as developer;
 class MapSearchProvider extends ChangeNotifier {
   List<Place> _searchResult = [];
   List<Place> get searchResult => _searchResult;
-  List<Place> _searchHistory = [Place(title: 'sdfsdf'), Place(title: 'fdsfsd')];
+  final List<Place> _tempData = [
+    Place(title: '한국공학대학교', latitude: '126.733926', longitude: '37.340370'),
+    Place(title: '산본 세종공원', latitude: '126.927293', longitude: '37.358323')
+  ];
 
   setSearchResult(String title) async {
-    _searchResult = _searchHistory;
+    _searchResult = _tempData;
     developer.log('searchResult: ${_searchResult.length}');
     // _searchResult = (await NaverMapService().getPlaces(title)) ?? [];
     notifyListeners();
