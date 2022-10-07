d07fd5adfa17484dfe0745fbc314120fcdc50435
hyejinxx <haejinjung1110@gmail.com>
2022-10-07 15:47:20 +0900

    feat: 라이딩 provider 추가

---
+++ lib/src/service/firebase_database_service.dart
@@ -0,0 +1,6 @@
+class FirebaseDatabaseService {
+  Future<void> saveRealTimeRecord(
+      String ridingDate, num ridingTime, num distance) async {}
+  // Future<void> saveFinalRecord(
+  //     String ridingDate, num ridingTime, num distance) async {}
+}
