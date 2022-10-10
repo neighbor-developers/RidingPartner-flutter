1bf708960175ebb9a6a2a88b36f7313ecafea1e6
SeoYeonn05 <seoyeonn051@gmail.com>
2022-10-10 14:18:09 +0900

    fix: marker 클래스 삭제

--- lib/src/pages/rental_map.dart
+++ lib/src/pages/rental_map.dart
@@ -20,12 +20,12 @@ class RentalMap extends StatefulWidget{
 
 class RentalMapState extends State<RentalMap>{
   final Completer<GoogleMapController> _controller = Completer();
-  List<MarkerInfo> location = [
-    // MarkerInfo("정왕 자전거 대여소", LatLng(37.343991285297, 126.74729588817),
-    //     "월 ~ 금\n(07시 ~ 21시)\n토요일, 일요일, 공휴일 휴무\n☎ 031-433-0101"),
-    // MarkerInfo("월곧 자전거 대여소", LatLng(37.3917953, 126.742692),
-    //     "수 ~ 일\n(09시 ~ 20시)\n월요일, 화요일, 공휴일 휴무\n☎ 031-433-0101")
-  ];
+  // List<MarkerInfo> location = [
+  //   MarkerInfo("정왕 자전거 대여소", LatLng(37.343991285297, 126.74729588817),
+  //       "월 ~ 금\n(07시 ~ 21시)\n토요일, 일요일, 공휴일 휴무\n☎ 031-433-0101"),
+  //   MarkerInfo("월곧 자전거 대여소", LatLng(37.3917953, 126.742692),
+  //       "수 ~ 일\n(09시 ~ 20시)\n월요일, 화요일, 공휴일 휴무\n☎ 031-433-0101")
+  // ];
   List<Marker> _markers = [
     // Marker(markerId: "rentalLocation_1",
     // draggable: true,
