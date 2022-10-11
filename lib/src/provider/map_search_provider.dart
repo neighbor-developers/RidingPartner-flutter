a65fac9e5c76fcd89922867a17e418cfe91efb50
bjk1649 <qhwns1649@naver.com>
2022-10-11 16:24:13 +0900

    feat: 출발지 도착지 설정기능 완성

--- lib/src/provider/map_search_provider.dart
+++ lib/src/provider/map_search_provider.dart
@@ -4,17 +4,45 @@ import '../service/naver_map_service.dart';
 import 'dart:developer' as developer;
 
 class MapSearchProvider extends ChangeNotifier {
-  List<Place> _searchResult = [];
-  List<Place> get searchResult => _searchResult;
-  final List<Place> _tempData = [
-    Place(title: '한국공학대학교', latitude: '126.733926', longitude: '37.340370'),
-    Place(title: '산본 세종공원', latitude: '126.927293', longitude: '37.358323')
-  ];
+  List<Place> _startPointSearchResult = [];
+  List<Place> get startPointSearchResult => _startPointSearchResult;
 
-  setSearchResult(String title) async {
-    _searchResult = _tempData;
-    developer.log('searchResult: ${_searchResult.length}');
-    // _searchResult = (await NaverMapService().getPlaces(title)) ?? [];
+  List<Place> _endPointSearchResult = [];
+  List<Place> get endPointSearchResult => _endPointSearchResult;
+
+  Place? _startPoint;
+  Place? get startPoint => _startPoint;
+
+  Place? _EndPoint;
+  Place? get EndPoint => _EndPoint;
+
+  setStartPoint(Place place) {
+    _startPoint = place;
+    notifyListeners();
+  }
+
+  setEndPoint(Place place) {
+    _EndPoint = place;
+    notifyListeners();
+  }
+
+  setStartPointSearchResult(String title) async {
+    _startPointSearchResult = (await NaverMapService().getPlaces(title)) ?? [];
+    notifyListeners();
+  }
+
+  setEndPointSearchResult(String title) async {
+    _endPointSearchResult = (await NaverMapService().getPlaces(title)) ?? [];
+    notifyListeners();
+  }
+
+  clearStartPointSearchResult() {
+    _startPointSearchResult = [];
+    notifyListeners();
+  }
+
+  clearEndPointSearchResult() {
+    _endPointSearchResult = [];
     notifyListeners();
   }
 }
