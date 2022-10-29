a8e438fac6322899a62c54c08cbf7971d5fba883
itaebro <plus100kt@gmail.com>
2022-10-29 14:14:15 +0900

    feat: 추천경로 기본 UI

--- lib/src/pages/recommended_route_page.dart
+++ lib/src/pages/recommended_route_page.dart
@@ -1,31 +1,89 @@
 import 'package:flutter/material.dart';
 import 'dart:developer' as developer;
 
-class RecommendedRoutePage extends StatelessWidget {
+import 'package:provider/provider.dart';
+import 'package:ridingpartner_flutter/src/provider/route_list_provider.dart';
+
+class RecommendedRoutePage extends StatefulWidget {
   const RecommendedRoutePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final List<String> entries = <String>['A', 'B', 'C'];
-    final List<int> colorCodes = <int>[600, 500, 100];
+  State<StatefulWidget> createState() => RecommendedRoutePageState();
+}
 
-    developer.log('recommend build');
+class RecommendedRoutePageState extends State<StatefulWidget> {
+  @override
+  Widget build(BuildContext context) {
+    final routeListProvider = Provider.of<RouteListProvider>(context);
+    final state = routeListProvider.state;
+
+    if (state == RouteListState.searching) {
+      routeListProvider.getRouteList();
+    }
+
+    Widget imageContainer(image) => Container(
+        margin: const EdgeInsets.all(5.0),
+        width: 50,
+        child: Image.network(image));
+
+    Widget textContainer(text) =>
+        Container(margin: const EdgeInsets.all(5.0), child: Text(text));
+
+    Widget routeListWidget() {
+      if (state == RouteListState.searching) {
+        return Container(
+          height: 100,
+          alignment: Alignment.center,
+          child: const Text(
+            'Loding',
+            style: TextStyle(fontSize: 30),
+          ),
+        );
+      } else if (state == RouteListState.empty) {
+        return Container(
+          height: 100,
+          alignment: Alignment.center,
+          child: const Text(
+            'Empty',
+            style: TextStyle(fontSize: 30),
+          ),
+        );
+      } else {
+        final routeList = routeListProvider.routeList;
+        return Flexible(
+          fit: FlexFit.tight,
+          child: ListView.separated(
+            padding: const EdgeInsets.all(8),
+            itemCount: routeList.length,
+            itemBuilder: (BuildContext context, int index) {
+              return SizedBox(
+                height: 50,
+                child: Row(children: [
+                  imageContainer(routeList[index].image!),
+                  textContainer(routeList[index].title!)
+                ]),
+              );
+            },
+            separatorBuilder: (BuildContext context, int index) =>
+                const Divider(),
+          ),
+        );
+      }
+    }
+
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('추천 경로'),
+        body: Column(
+      children: <Widget>[
+        Container(
+          height: 100,
+          alignment: Alignment.center,
+          child: const Text(
+            '# 추천 경로',
+            style: TextStyle(fontSize: 30),
+          ),
         ),
-        body: ListView.separated(
-          padding: const EdgeInsets.all(8),
-          itemCount: entries.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Container(
-              height: 50,
-              color: Colors.amber[colorCodes[index]],
-              child: Center(child: Text('Entry ${entries[index]}')),
-            );
-          },
-          separatorBuilder: (BuildContext context, int index) =>
-              const Divider(),
-        ));
+        routeListWidget(),
+      ],
+    ));
   }
 }
--- lib/src/provider/route_list_provider.dart
+++ lib/src/provider/route_list_provider.dart
@@ -1,3 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:ridingpartner_flutter/src/models/route.dart';
+import 'package:ridingpartner_flutter/src/service/firestore_service.dart';
 
-class RouteListProvider with ChangeNotifier {}
+enum RouteListState { searching, empty, completed, click }
+
+class RouteListProvider with ChangeNotifier {
+  RouteListState _state = RouteListState.searching;
+  RouteListState get state => _state;
+
+  late final List<RidingRoute> _routeList;
+  List<RidingRoute> get routeList => _routeList;
+
+  Future<void> getRouteList() async {
+    try {
+      if (_state == RouteListState.searching) {
+        _routeList = await FireStoreService().getRoutes();
+
+        if (_routeList.isEmpty) {
+          _state = RouteListState.empty;
+        } else {
+          _state = RouteListState.completed;
+        }
+      }
+      notifyListeners();
+    } catch (e) {
+      _state = RouteListState.empty;
+    }
+  }
+}
