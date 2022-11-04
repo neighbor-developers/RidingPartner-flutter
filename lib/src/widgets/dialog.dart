4a173eb6d8f2675bcf5bc51c63400d5a5b1e06d0
hyejinxx <haejinjung1110@gmail.com>
2022-11-04 17:50:09 +0900

    feat: 기본 dialog 위젯 추가

---
+++ lib/src/widgets/dialog.dart
@@ -0,0 +1,27 @@
+import 'dart:js';
+import 'package:flutter/material.dart';
+
+Future<bool> backDialog(BuildContext context, String text) async {
+  return await showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) => AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0)),
+            title: Column(children: [Text("뒤로가기")]),
+            content: Column(children: [Text(text)]),
+            actions: <Widget>[
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context, false);
+                  },
+                  child: Text("취소")),
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context, true);
+                    Navigator.pop(context, true);
+                  },
+                  child: Text("확인")),
+            ],
+          ));
+}
