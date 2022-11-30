d3c0c66d2a8879ef063b7b1998001e2d9a5ccd75
mi-fasol <minyoong39@naver.com>
2022-11-30 15:11:11 +0900

    fix: 로그인 UI 수정

--- lib/src/pages/loding_page.dart
+++ lib/src/pages/loding_page.dart
@@ -10,9 +10,7 @@ import 'package:ridingpartner_flutter/src/provider/auth_provider.dart';
 import 'package:ridingpartner_flutter/src/provider/bottom_navigation_provider.dart';
 import 'package:ridingpartner_flutter/src/provider/home_record_provider.dart';
 import 'package:ridingpartner_flutter/src/provider/map_search_provider.dart';
-import 'package:ridingpartner_flutter/src/provider/navigation_provider.dart';
 import 'package:ridingpartner_flutter/src/provider/place_list_provider.dart';
-import 'package:ridingpartner_flutter/src/provider/riding_provider.dart';
 import 'package:ridingpartner_flutter/src/provider/route_list_provider.dart';
 import 'package:ridingpartner_flutter/src/provider/sights_provider.dart';
 import 'package:ridingpartner_flutter/src/provider/weather_provider.dart';
@@ -27,6 +25,7 @@ class LodingPage extends StatefulWidget {
 class _LodingPageState extends State<LodingPage> {
   late ConnectivityResult connectivityResult;
   late AuthProvider _authProvider;
+
   @override
   void initState() {
     super.initState();
@@ -85,9 +84,9 @@ class _LodingPageState extends State<LodingPage> {
               fit: BoxFit.cover),
         ),
         child: Container(
-            margin: const EdgeInsets.symmetric(vertical: 30.0),
+            margin: const EdgeInsets.fromLTRB(40.0, 20.0, 40.0, 0.0),
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 const Text(
                   "RIDING PARTNER\nIN SIHEUNG",
@@ -108,52 +107,111 @@ class _LodingPageState extends State<LodingPage> {
   Widget _naverLoginButton() {
     return SizedBox(
         width: MediaQuery.of(context).size.width,
-        child: Visibility(
-            visible: _authProvider.user == null,
-            child: CupertinoButton(
-                onPressed: () {
-                  _authProvider.signInWithNaver();
-                },
-                color: Color.fromRGBO(62, 200, 76, 1),
-                child: Text('네이버 로그인',
-                    style: TextStyle(
-                      fontSize: 15,
-                      color: Colors.black87,
-                    )))));
+        height: 55.0,
+        child: Container(
+            margin: const EdgeInsets.only(top: 10.0),
+            child: Visibility(
+                visible: _authProvider.user == null,
+                child: ElevatedButton(
+                  onPressed: () {
+                    _authProvider.signInWithNaver();
+                  },
+                  style: ElevatedButton.styleFrom(backgroundColor: const Color.fromRGBO(62, 200, 76, 1)),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Icon(
+                        Icons.messenger,
+                        color: Colors.white,
+                        size: 22.0,
+                      ),
+                      Text('네이버 로그인',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.white,
+                              fontWeight: FontWeight.bold
+                          )),
+                      Icon(
+                        Icons.arrow_forward_ios_outlined,
+                        color: Colors.brown,
+                        size: 14.0,
+                      )
+                    ],
+                  ),
+                ))));
   }
 
   Widget _kakaoLoginButton() {
     return SizedBox(
         width: MediaQuery.of(context).size.width,
-        child: Visibility(
-            visible: _authProvider.user == null,
-            child: CupertinoButton(
-                onPressed: () {
-                  _authProvider.signInWithKakao();
-                },
-                color: Colors.yellow,
-                // ignore: prefer_const_constructors
-                child: Text('카카오 로그인',
-                    style: TextStyle(
-                      fontSize: 15,
-                      color: Colors.black87,
+        height: 65.0,
+        child: Container(
+            margin: const EdgeInsets.only(top: 20),
+            child: Visibility(
+                visible: _authProvider.user == null,
+                child: ElevatedButton(
+                    onPressed: () {
+                      _authProvider.signInWithKakao();
+                    },
+                    style: ElevatedButton.styleFrom(backgroundColor: Colors.yellow),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Icon(
+                          Icons.messenger,
+                          color: Colors.brown,
+                          size: 22.0,
+                        ),
+                        // ignore: prefer_const_constructors
+                        Text('카카오 로그인',
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.black87,
+                                fontWeight: FontWeight.bold
+                            )),
+                        Icon(
+                          Icons.arrow_forward_ios_outlined,
+                          color: Colors.brown,
+                          size: 14.0,
+                        )
+                      ],
                     )))));
   }
 
   Widget _googleLoginButton() {
     return SizedBox(
         width: MediaQuery.of(context).size.width,
-        child: Visibility(
-            visible: _authProvider.user == null,
-            child: CupertinoButton(
-                onPressed: () {
-                  _authProvider.signInWithGoogle();
-                },
-                color: Color.fromARGB(255, 255, 255, 255),
-                child: Text('구글 로그인',
-                    style: TextStyle(
-                      fontSize: 15,
-                      color: Colors.black87,
+        height: 55.0,
+        child: Container(
+            margin: const EdgeInsets.only(top: 10.0),
+            child: Visibility(
+                visible: _authProvider.user == null,
+                child: ElevatedButton(
+                    onPressed: () {
+                      _authProvider.signInWithGoogle();
+                    },
+                    style: ElevatedButton.styleFrom(backgroundColor: Colors.white),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Icon(
+                          Icons.messenger,
+                          color: Colors.brown,
+                          size: 22.0,
+                        ),
+                        Text('구글 로그인',
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Color.fromARGB(150, 40, 40, 40),
+                              fontWeight: FontWeight.bold
+                            )),
+                        Icon(
+                          Icons.arrow_forward_ios_outlined,
+                          color: Colors.brown,
+                          size: 14.0,
+                        )
+                      ],
                     )))));
   }
 
@@ -161,17 +219,36 @@ class _LodingPageState extends State<LodingPage> {
     if (Platform.isIOS) {
       return SizedBox(
           width: MediaQuery.of(context).size.width,
-          child: Visibility(
-              visible: _authProvider.user == null,
-              child: CupertinoButton(
-                  onPressed: () {
-                    _authProvider.signInWithApple();
-                  },
-                  color: Color.fromARGB(255, 2, 2, 2),
-                  child: Text('애플 로그인',
-                      style: TextStyle(
-                        fontSize: 15,
-                        color: Colors.white,
+          height: 55.0,
+          child: Container(
+              margin: const EdgeInsets.only(top: 10.0),
+              child: Visibility(
+                  visible: _authProvider.user == null,
+                  child: ElevatedButton(
+                      onPressed: () {
+                        _authProvider.signInWithApple();
+                      },
+                      style: ElevatedButton.styleFrom(backgroundColor: const Color.fromARGB(255, 2, 2, 2)),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: const [
+                            Icon(
+                              Icons.messenger,
+                              color: Colors.brown,
+                              size: 22.0,
+                            ),
+                            Text('애플 로그인',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  color: Colors.white,
+                                    fontWeight: FontWeight.bold
+                                )),
+                            Icon(
+                              Icons.arrow_forward_ios_outlined,
+                              color: Colors.brown,
+                              size: 14.0,
+                            )
+                          ]
                       )))));
     } else {
       return Column();
