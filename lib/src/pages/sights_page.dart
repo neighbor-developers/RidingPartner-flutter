9ce4a9e6b40fef8cc7803efee23fa535bbf920d7
SeoYeonn05 <seoyeonn051@gmail.com>
2022-12-23 03:33:43 +0900

    fix: bottomSheet 수정

--- lib/src/pages/sights_page.dart
+++ lib/src/pages/sights_page.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -9,7 +10,10 @@ import 'package:ridingpartner_flutter/src/utils/custom_marker.dart';
 import '../models/place.dart';
 import '../provider/navigation_provider.dart';
 import '../provider/riding_provider.dart';
+import 'home_page.dart';
 import 'navigation_page.dart';
+import 'package:flutter/src/painting/rounded_rectangle_border.dart';
+import 'dart:developer' as developer;
 
 class SightsPage extends StatelessWidget {
   final Completer<GoogleMapController> _controller = Completer();
@@ -21,77 +25,111 @@ class SightsPage extends StatelessWidget {
 
     final state = sightsProvider.state;
 
-    void routeDialog(Place place) => showDialog(
+    void routeDialog(Place place) => showModalBottomSheet<void>(
         context: context,
         //barrierDismissible - Dialog를 제외한 다른 화면 터치 x
-        barrierDismissible: false,
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(15.0),
+                topRight: Radius.circular(15.0))),
         builder: (BuildContext context) {
           double height = MediaQuery.of(context).size.height;
           double width = MediaQuery.of(context).size.width;
+          developer.log("높이: $height");
+          developer.log("넓이 : $width");
 
-          return Container(
-              height: height * 0.5,
-              width: width * 0.9,
-              child: AlertDialog(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0)),
-                //Dialog Main Title
-                title: Text(style: TextStyle(), place.title!),
-                content: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Row(
-                        //mainAxisAlignment: MainAxisAlignment.start,
-                        children: <Widget>[
-                          Text(place.roadAddress ?? "위치 로드에 실패하였습니다. 재접속해주세요",
-                              style: const TextStyle(
-                                color: Color(0xFFE9E9E9),
-                              )),
-                          //Text(sightsProvider.distance+"km")
-                        ]),
-                    Image.asset(place.image!),
-                  ],
+          return Column(
+              children: [
+                Container(
+                  height: height*0.5 - height*0.07,
+                  padding: const EdgeInsets.fromLTRB(20, 30, 20, 0),
+                  child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    //padding: const EdgeInsets.fromLTRB(20, 30, 20, 0),
+                      children: [
+                        Text(style: const TextStyle(
+                          fontSize: mainFontSize,
+                          color: Color.fromRGBO(51, 51, 51, 1),
+                          fontFamily: "Pretendard",
+                          height: 1.3,
+                          letterSpacing: 0.02,
+                          fontWeight: FontWeight.w800), place.title!),
+                        // Column(
+                        //   mainAxisSize: MainAxisSize.min,
+                        //   crossAxisAlignment: CrossAxisAlignment.start,
+                        //   children: <Widget>[
+                        //     Row(
+                        //       //mainAxisAlignment: MainAxisAlignment.start,
+                        //         children: <Widget>[
+                        //           Text(place.roadAddress ?? "위치 로드에 실패하였습니다. 재접속해주세요",
+                        //               style: const TextStyle(
+                        //                 color: Color(0xFFE9E9E9),
+                        //               )),
+                        //           //Text(sightsProvider.distance+"km")
+                        //         ]),
+                        //     Image.asset(place.image!),
+                        //   ],
+                        Text(place.roadAddress ?? "위치 로드에 실패하였습니다. 재접속해주세요",
+                            style: const TextStyle(
+                              fontSize: recordFontSize,
+                              color: Color(0xFF999999),
+                            )),
+                        const Divider(color: Colors.grey, thickness: 1.0),
+                        Card(
+                            semanticContainer: true,
+                            clipBehavior: Clip.antiAliasWithSaveLayer,
+                            shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12.0)
+                          ),
+                          child: Image.asset(place.image!) 
+                        )
+                        
+
+                    // ),]
+  /*          ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(const Color(0xFFE9E9E9)),
+                  //maximumSize: Size(),
                 ),
-                actions: <Widget>[
-                  ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                          const Color(0xFFE9E9E9)),
-                      //maximumSize: Size(),
-                    ),
-                    child: const Text(
-                        style: TextStyle(color: Color(0xFF666666)), "취소"),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                  ElevatedButton(
-                    style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                            Colors.orange[600]!)),
-                    child: const Text("안내 시작",
-                        style: TextStyle(color: Colors.white)),
-                    onPressed: () async {
-                      final placeList = <Place>[place];
+                child: const Text(style: TextStyle(color: Color(0xFF666666)), "취소"),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),*/]
+                  )
+                ),
+                SizedBox(
+                    height: height*0.07,
+                    width: width,
+                    child:  ElevatedButton(
+                      style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(Colors.orange[1100]!)),
+                      child: const Text("안내 시작",
+                          style: TextStyle(color: Colors.white)),
+                      onPressed: () async {
+                        final placeList = <Place>[place];
 
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => MultiProvider(
-                                    providers: [
-                                      ChangeNotifierProvider(
-                                          create: (context) =>
-                                              NavigationProvider(placeList)),
-                                      ChangeNotifierProvider(
-                                          create: (context) => RidingProvider())
-                                    ],
-                                    child: NavigationPage(),
-                                  )));
-                    },
-                  ),
-                ],
-              ));
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => MultiProvider(
+                                  providers: [
+                                    ChangeNotifierProvider(
+                                        create: (context) =>
+                                            NavigationProvider(placeList)),
+                                    ChangeNotifierProvider(
+                                        create: (context) => RidingProvider())
+                                  ],
+                                  child: NavigationPage(),
+                                )));
+                      },
+                    )
+                ),
+            ],
+          );
         });
 
     if (state == MarkerListState.searching) {
