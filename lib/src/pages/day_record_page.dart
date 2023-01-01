ac8f988515b1a7cba90650ccef3194ff271c0fde
mi-fasol <minyoong39@naver.com>
2023-01-01 15:48:47 +0900

    fix: day_record_page 수정

--- lib/src/pages/day_record_page.dart
+++ lib/src/pages/day_record_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../models/record.dart';
+import '../provider/home_record_provider.dart';
 import '../provider/riding_result_provider.dart';
+import '../utils/timestampToText.dart';
 
 class DayRecordPage extends StatelessWidget {
   DayRecordPage({super.key});
@@ -9,6 +13,25 @@ class DayRecordPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Record _record = Record();
+    _recordProvider = Provider.of<RidingResultProvider>(context);
+    num _speed = 0;
+
+    if (_recordProvider.recordState == RecordState.loading) {
+      _recordProvider.getRidingData();
+    }
+
+    if (_recordProvider.recordState == RecordState.success) {
+      _record = _recordProvider.record;
+      if (_record.distance != null) {
+        _speed = _record.distance as num;
+        _speed = _speed / 3 * 3600;
+      }
+    }
+
+    _record.memo ??= "그 날의 메모가 존재하지 않습니다.";
+    _record.kcal ??= 0.0;
+
     return Scaffold(
       appBar: AppBar(
         shadowColor: const Color.fromRGBO(255, 255, 255, 0.5),
@@ -43,7 +66,9 @@ class DayRecordPage extends StatelessWidget {
           height: 240,
           child: Image(
               image:
-              AssetImage('assets/images/places/gaetgol_park.jpeg'))),
+              AssetImage('assets/images/img_loading.png'),
+              fit: BoxFit.fill
+          )),
       Container(
         margin: const EdgeInsets.only(left: 24.0, right: 24.0, top: 30),
         child: Row(
@@ -94,43 +119,49 @@ class DayRecordPage extends StatelessWidget {
             ),
             SizedBox(
               height: 120.0,
-              width: 130.0,
+              width: 220.0,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.start,
-                children: const [
+                children: [
                   Text(
-                    "2022년 12월 6일",
-                    style:  TextStyle(
+                    _record.date!,
+                    style: const TextStyle(
                       fontFamily: "Pretendard",
                       fontSize: 16.0,
                       color: Color(0xFF666666),
                     ),
                   ),
                   Text(
-                    "01:10:02",
-                    style:  TextStyle(
+                    timestampToText(_record.timestamp!),
+                    style: const TextStyle(
                         fontSize: 16.0,
                         fontFamily: "Pretendard",
-                        color: Color(0xFF666666)),
+                        fontWeight: FontWeight.w500,
+                        color:
+                        Color(0xFF666666)),
                   ),
                   Text(
-                    "24 km/h",
-                    style:  TextStyle(
+                    "${_record.distance! / _record.timestamp!} km/h",
+                    style: const TextStyle(
                         fontSize: 16.0,
                         fontFamily: "Pretendard",
-                        color: Color(0xFF666666)),
+                        fontWeight: FontWeight.w500,
+                        color:
+                        Color(0xFF666666)),
                   ),
                   Text(
-                    "16 km",
-                    style:  TextStyle(
+                    "${_record.distance! / 1000} km",
+                    style: const TextStyle(
                         fontSize: 16.0,
                         fontFamily: "Pretendard",
-                        color: Color(0xFF666666)),
+                        fontWeight: FontWeight.w500,
+                        color:
+                        Color(0xFF666666)),
                   ),
                   Text(
-                    "600 kcal",
-                    style:  TextStyle(
+                    "${_record.kcal.toString()} kcal",
+                    style: const TextStyle(
                         fontSize: 16.0,
                         fontFamily: "Pretendard",
                         color: Color(0xFF666666)),
@@ -154,7 +185,7 @@ class DayRecordPage extends StatelessWidget {
           margin: const EdgeInsets.only(left: 24.0, right: 24.0),
           padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 12.0),
           child: Text(
-          "뭐시기 저시기 기록에 있는 메모 뭐시기 저시기 기록에 있는 메모 뭐시기 저시기 기록에 있는 메모 뭐시기 저시기 기록에 있는 메모",
+          _record.memo!,
           style: TextStyle(
       fontSize: 14.0,
       color: const Color(0x333333).withOpacity(0.8),
