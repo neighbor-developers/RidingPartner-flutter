833056ef6e32509cc6c2953413e0a40ce34a01fe
hyejinxx <haejinjung1110@gmail.com>
2023-01-22 23:32:55 +1100

    feat: 탈퇴버튼 클릭시 다이얼로그 노출

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -390,17 +390,7 @@ class _HomePageState extends State<HomePage>
         ),
         const Text('   |   '),
         InkWell(
-          onTap: () async {
-            await _settingProvider.withdrawal();
-            Navigator.pushAndRemoveUntil(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => ChangeNotifierProvider(
-                          create: (context) => AuthProvider(),
-                          child: const LodingPage(),
-                        )),
-                ((route) => false));
-          },
+          onTap: () => withdrawalDialog(),
           child: Text('계정 탈퇴', style: settingTextStyle),
         ),
       ],
@@ -941,5 +931,107 @@ class _HomePageState extends State<HomePage>
         }
       }
     });
+  }
+
+  Future<bool> withdrawalDialog() async {
+    return await showDialog(
+        context: context,
+        barrierDismissible: false,
+        useRootNavigator: false,
+        builder: (BuildContext context) => AlertDialog(
+              buttonPadding: const EdgeInsets.all(20),
+              alignment: Alignment.center,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0)),
+              content: Container(
+                  margin: const EdgeInsets.only(top: 20),
+                  child: const Text.rich(
+                    TextSpan(
+                        text: '계정 탈퇴하시겠습니까?\n',
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 24, 24, 1),
+                            fontFamily: 'Pretendard',
+                            fontWeight: FontWeight.w600,
+                            fontSize: 17),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: '(모든 기록이 삭제됩니다)',
+                            style: TextStyle(
+                                color: Color.fromARGB(150, 24, 24, 1),
+                                fontFamily: 'Pretendard',
+                                height: 1.5,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 13),
+                          )
+                        ]),
+                    textAlign: TextAlign.center,
+                  )),
+              actions: <Widget>[
+                SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: 55,
+                    child: Row(
+                      children: [
+                        Flexible(
+                            child: InkWell(
+                          child: Container(
+                            margin: const EdgeInsets.fromLTRB(0, 0, 6, 0),
+                            alignment: Alignment.center,
+                            height: 55,
+                            decoration: BoxDecoration(
+                              color: const Color.fromRGBO(234, 234, 234, 1),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: const Text(
+                              '취소',
+                              style: TextStyle(
+                                  color: Color.fromRGBO(102, 102, 102, 1),
+                                  fontFamily: 'Pretendard',
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 16),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                          onTap: () {
+                            Navigator.pop(context, false);
+                          },
+                        )),
+                        Flexible(
+                            child: InkWell(
+                          child: Container(
+                            margin: EdgeInsets.fromLTRB(6, 0, 0, 0),
+                            alignment: Alignment.center,
+                            height: 55,
+                            decoration: BoxDecoration(
+                              color: const Color.fromRGBO(240, 120, 5, 1),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: const Text(
+                              '탈퇴',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: 'Pretendard',
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 16),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                          onTap: () async {
+                            await _settingProvider.withdrawal();
+                            Navigator.pushAndRemoveUntil(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        ChangeNotifierProvider(
+                                          create: (context) => AuthProvider(),
+                                          child: const LodingPage(),
+                                        )),
+                                ((route) => false));
+                          },
+                        ))
+                      ],
+                    ))
+              ],
+            ));
   }
 }
