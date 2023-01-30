6b291eb9d424db4f5748c87109ece403f3e3bbc7
mi-fasol <minyoong39@naver.com>
2023-01-30 09:47:22 +0900

    fix: home_page 명소 다이얼로그 수정

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -156,7 +156,7 @@ class _HomePageState extends State<HomePage>
                       const SizedBox(
                         height: 8,
                       ),
-                      if (place.roadAddress == null) ...[
+                      if (place.roadAddress == null || place.roadAddress == "") ...[
                         Text(
                           place.jibunAddress!,
                           style: const TextStyle(
