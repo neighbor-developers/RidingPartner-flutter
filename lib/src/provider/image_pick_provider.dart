a4f9d3734f8e443a467ee37a7210393ea7be2653
hyejinxx <haejinjung1110@gmail.com>
2023-04-10 14:31:04 +0900

    refactor: imagePicker 서비스 프로바이더 분리

---
+++ lib/src/provider/image_pick_provider.dart
@@ -0,0 +1,46 @@
+import 'dart:io';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:image_picker/image_picker.dart';
+
+import '../service/image_picker_service.dart';
+
+class ImageState extends StateNotifier<List<File>> {
+  ImageState() : super(<File>[]);
+  final ImagePickerService picker = ImagePickerService();
+
+  @override
+  set state(List<File> value) {
+    super.state = value;
+  }
+
+  delImage(File image) {
+    var list = [...super.state];
+    list.remove(image);
+    state = list;
+  }
+
+  void addImage(List<File> value) {
+    var list = [...super.state];
+    if (list.isEmpty) {
+      state = value;
+    } else {
+      list.addAll(value);
+      list.toSet().toList();
+      state = list;
+    }
+    if (super.state.length > 4) {
+      state = super.state.sublist(0, 4);
+      Fluttertoast.showToast(msg: 'You can only upload 5 images');
+    }
+  }
+
+  Future getImage() async {
+    picker.pickImage().then((value) {
+      addImage(value);
+    }).catchError((onError) {
+      Fluttertoast.showToast(msg: 'failed to get image');
+    });
+  }
+}
---
+++ lib/src/service/image_picker_service.dart
@@ -0,0 +1,62 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class ImagePickerService {
+  static final ImagePickerService _imagePickerService =
+      ImagePickerService._internal();
+  factory ImagePickerService() {
+    return _imagePickerService;
+  }
+  ImagePickerService._internal();
+
+  final FilePicker _filePicker = FilePicker.platform;
+  Future<List<File>> pickImage() async {
+    confirmPermissionGranted();
+    try {
+      final pickedFile = await _filePicker.pickFiles(
+        type: FileType.image,
+        allowMultiple: true,
+      );
+      if (pickedFile != null) {
+        return pickedFile.files.map((e) => File(e.path!)).toList();
+      } else {
+        return [];
+      }
+    } catch (e) {
+      print('ImagePickerService: $e');
+      return [];
+    }
+  }
+
+  Future<String?> pickSingleImage() async {
+    try {
+      confirmPermissionGranted();
+      final pickedFile = await _filePicker.pickFiles(
+        type: FileType.image,
+        allowMultiple: false,
+      );
+      if (pickedFile != null) {
+        return pickedFile.files.first.path;
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print('ImagePickerService: $e');
+      return null;
+    }
+  }
+
+  Future<void> confirmPermissionGranted() async {
+    // storage와  camera의 권한을 요청
+    PermissionStatus storageStatus = await Permission.storage.request();
+    PermissionStatus cameraStatus = await Permission.camera.request();
+
+    if (!storageStatus.isGranted || !cameraStatus.isGranted) {
+      Fluttertoast.showToast(msg: "권한을 허용해주세요");
+      return;
+    }
+  }
+}
